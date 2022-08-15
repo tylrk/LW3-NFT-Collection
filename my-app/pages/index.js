f9d37856e01d348004431f0dd40cff40599cca0b
tylrk <tylerk1117@gmail.com>
2022-08-15 18:53:44 -0500

    fix

--- my-app/pages/index.js
+++ my-app/pages/index.js
@@ -322,7 +322,7 @@ export default function Home() {
         <div>
           <h1 className={styles.title}>Welcome to CryptoDevs!</h1>
           <div className={styles.description}>
-            It's an NFT collection for developers in Crypto.
+            It is an NFT collection for developers in Crypto.
           </div>
           <div className={styles.description}>
             {tokenIdsMinted}/20 have been minted
